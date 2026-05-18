b7c286978af4917dac5bc6f5a1285fe8cc287fd6
SergioRuizm <sergio5ruizmontesinos@gmail.com>
2026-05-18 08:14:01 +0200

    Delete demo directory

--- demo/src/main/java/com/lovecode/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package com.lovecode;
- 
-import org.springframework.data.jpa.repository.JpaRepository;
- 
-public interface UsuarioRepository extends JpaRepository<usuario, Long> {
-    usuario findByEmail(String email);
-}
-    
